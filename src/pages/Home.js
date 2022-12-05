55d9d69694183f4f8608284925216981fe2bd25b
Corey Schadeck <corey@schadeck.com>
2022-12-05 13:01:05 -0500

    changed Home Page alignment and fixed color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,30 +11,26 @@ export default function Home() {
         <div className="home">
             <div className="about">
                 <h2>Hello, my name is Corey</h2>
-                <div className="prompt"><p>I am a Software Developer with a passion for creating and learning. I have a successful  background in management, health and fitness. I look forward to working with a company who promotes growth and creative thinking. </p>
+                <div className="prompt"><p>I am a Software Developer with a passion for creating and learning. I have an eye for detail and an impulse for organization. I thrive when working within a creative, collaborative team. </p>
                     <a href="https://www.linkedin.com/in/coreyschadeck" target="_blank" rel="noreferrer"><AiOutlineLinkedin /></a>
-                   <a href="https://github.com/CSchadeck" target="_blank" rel="noreferrer"> <AiOutlineGithub /></a>
-                   <a href="mailto:corey@schadeck.com">< AiOutlineMail /> </a>
+                    <a href="https://github.com/CSchadeck" target="_blank" rel="noreferrer"> <AiOutlineGithub /></a>
+                    <a href="mailto:corey@schadeck.com">< AiOutlineMail /> </a>
                 </div>
             </div>
 
             <div className="skills"><h1>Skills</h1>
                 <ol className="list">
                     <li className="item">
-                        <h2> Front-End</h2>
-                        <span>ReactJS, HTML, CSS</span>
+                        <h2> Front-End Languages</h2>
+                        <span>Javascript, ReactJS, HTML, CSS</span>
                     </li>
                     <li className="item">
-                        <h2> Back-End</h2>
-                        <span>Ruby On Rails, MySql, </span>
-                    </li>
-                    <li className="item">
-                        <h2> Languages</h2>
-                        <span>JavaScrip, Ruby</span>
+                        <h2> Back-End Languages</h2>
+                        <span>Ruby On Rails, Python, SQL, MySql, PostgreSQL </span>
                     </li>
                     <li className="item">
                         <h2> Other Skills</h2>
-                        <span>Regional Managment, Scheduling, Event Coordinating, Hiring, Onboarding, Communications</span>
+                        <span>Certified Scrum Master, Regional Management, Scheduling, Event Coordinating, Hiring, Onboarding, Public Speaking, Cross-Disciplinary Communications</span>
                     </li>
                 </ol>
             </div>
