c69c61fc7960d0f4b4a9aed7f167c39d758f4a92
Corey Schadeck <corey@schadeck.com>
2022-12-05 12:19:48 -0500

    added thought forum

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,3 +1,4 @@
+import ThoughtForum from "../assets/ThoughtForum.png"
 import ZenASketch from "../assets/ZenASketch.png"
 import CTravelJournal from "../assets/CTravelJournal.png"
 import BuddyandButtercup from "../assets/BuddyandButtercup.png"
@@ -6,24 +7,31 @@ import BuddyandButtercup from "../assets/BuddyandButtercup.png"
 
 export const ProjectList = [
     {
+        name: "Feelings Forum",
+        image: ThoughtForum,
+        skills: "React.JS, Python, HTML, CSS",
+        link: "https://thoughtforum.netlify.app"
+
+    },
+    {
         name: "Zen-A-Sketch",
         image: ZenASketch,
         skills: "Javascript, HTML, CSS",
-        link:"https://zen-a-sketch.netlify.app/"
+        link: "https://zen-a-sketch.netlify.app/"
 
     },
     {
         name: "Corey's Travel Journey",
         image: CTravelJournal,
         skills: "React-JS, HTML, CSS, ",
-        link:"https://coreystraveljournel.netlify.app/"
+        link: "https://coreystraveljournel.netlify.app/"
 
     },
     {
         name: "Buddy and Buttercup",
         image: BuddyandButtercup,
         skills: "HTML, CSS, Javascript: Event Listeners only",
-        link:"https://buddyandbuttercup.netlify.app/"
+        link: "https://buddyandbuttercup.netlify.app/"
 
     },
 
